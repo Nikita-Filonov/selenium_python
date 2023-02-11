aaabc0843dbfa340875103f643314f004081e83a
Nikita Filonov <63447343+Nikita-Filonov@users.noreply.github.com>
2023-02-11 21:00:11 +0300

    Update page.py

--- utils/types/webdriver/driver/page.py
+++ utils/types/webdriver/driver/page.py
@@ -1,12 +1,14 @@
 from abc import ABC, abstractmethod, abstractproperty
 from typing import TYPE_CHECKING, Union
+
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support.wait import WebDriverWait
 
 from config import UIConfig
 
-
 if TYPE_CHECKING:
+    from utils.types.webdriver.driver.element import ElementInterface
+    from utils.types.webdriver.driver.elements import ElementsInterface
     from utils.types.webdriver.driver.waiting import WaitingInterface
 
 
@@ -27,4 +29,48 @@ class PageInterface(ABC):
         use_self: bool = False,
         ignored_exceptions: list = None
     ) -> Union[WebDriverWait, "WaitingInterface"]:
+        ...
+
+    @abstractmethod
+    def init_webdriver(self) -> WebDriver:
+        ...
+
+    @abstractmethod
+    def title(self) -> str:
+        ...
+
+    @abstractmethod
+    def url(self) -> str:
+        ...
+
+    @abstractmethod
+    def visit(self, url: str) -> "PageInterface":
+        ...
+
+    @abstractmethod
+    def reload(self) -> "PageInterface":
+        ...
+
+    @abstractmethod
+    def get_xpath(self, xpath: str, timeout: int = None) -> ElementInterface:
+        ...
+
+    @abstractmethod
+    def find_xpath(self, xpath: str, timeout: int = None) -> ElementsInterface:
+        ...
+
+    @abstractmethod
+    def quit(self):
+        ...
+
+    @abstractmethod
+    def screenshot(self, filename: str) -> str:
+        ...
+
+    @abstractmethod
+    def maximize_window(self) -> "PageInterface":
+        ...
+
+    @abstractmethod
+    def execute_script(self, script: str, *args) -> "PageInterface":
         ...
